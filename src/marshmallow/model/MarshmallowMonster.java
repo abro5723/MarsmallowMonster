9e73ee030240921316ca68c7406abb17eaee252e
abro5723 <abro5723@740s-pl-114.local>
2015-09-11 09:22:15 -0600

    Changes
    I gave my monster a legend of sorts, just for the lols

--- src/marshmallow/model/MarshmallowMonster.java
+++ src/marshmallow/model/MarshmallowMonster.java
@@ -26,8 +26,12 @@ public class MarshmallowMonster
 	
 	public String toString()
 	{
-		String monster = "This monster has" + monsterEyes + "eyes, and its' name is" + monsterName;
-		
+		String monster = "This monster has " + monsterEyes + " eyes, and its' name is " + monsterName + ".";
+		System.out.println ("His legend starts in Grevwald, when his father was killed by his mother.");
+		System.out.println ("His mother had also killed most of his siblings, Spindle could not save any of his siblings, he tried valiantly, but failed.");
+		System.out.println ("The screams of his siblings haunted him for his entire life, he began to 'hear' things.");
+		System.out.println ("After he could no longer bear it, he dicide to find his mother and end her life.");
+		System.out.println ( "His" + monsterLegs + "  legs had been torn off by his mother.");
 		return monster;
 	}
 
